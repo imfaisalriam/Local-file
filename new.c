469fe799f37e7dd2a8d4bb80f77912e6319e6812
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-10-22 11:53:00 +0600

    No change here

--- new.c
+++ new.c
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 #include<stdio.h>
 int main()
 {
     printf("Hello World!\n");
 }
-=======
-#include<stdio.h>
-int main()
-{
-    printf("Hello World!\nAnd i'm riam\n");
-}
->>>>>>> 57b5d0ceb2a419fc4c108b676ef529fe72c30485
