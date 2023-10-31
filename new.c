e19a43d7ad4cbe72228de881573bfc7732c908d5
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-10-31 11:52:29 +0600

    write a differtent code in new.c file and deleted 2 unused file

--- new.c
+++ new.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int diff_of_time=265313;
-    int diff_day=diff_of_time/86400;
-    diff_of_time=diff_of_time%86400;
-    int diff_hour=diff_of_time/3600;
-    diff_of_time=diff_of_time%3600;
-    int diff_munite=diff_of_time/60;
-    int diff_second=diff_of_time%60;
-    printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n",diff_day,diff_hour,diff_munite,diff_second);
+    int min=0,max=0;
+    float avg=0;
+    int ar[6];
+    for (int  i = 0; i < 6; i++)
+    {
+        scanf("%d",&ar[i]);
+        min-=ar[i];
+        max+=ar[i];
+    }
+    avg=(float)max/6;
+    printf("%d   %d   %.2f",min,max,avg);
+    
+
 
 
     return 0;
