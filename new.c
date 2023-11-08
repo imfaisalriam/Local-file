921fe535d7c7f6bf0c2b527d2eecb7fdf69d13cd
Faisal Ahmed Riam <smfaisalriam@gmail.com>
2023-11-09 00:11:19 +0600

    changes

--- new.c
+++ new.c
@@ -1,32 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    char begin_day[5], end_day[5];
-    int begin_time[3], end_time[3];
+    int init_hours, init_minutes, init_seconds;
+    char init_time[] = "08 : 12 : 23";
 
-    // Input for the beginning day and time
-    scanf("Dia %d", &begin_time[0]);
-    scanf("%d : %d : %d", &begin_time[1], &begin_time[2], &begin_time[3]);
-    scanf(" Dia %d", &end_time[0]);
-    scanf("%d : %d : %d", &end_time[1], &end_time[2], &end_time[3]);
-
-    // Calculate the time in seconds for the beginning and ending times
-    long long begin_seconds = (begin_time[0] * 24 * 60 * 60) + (begin_time[1] * 60 * 60) + (begin_time[2] * 60) + begin_time[3];
-    long long end_seconds = (end_time[0] * 24 * 60 * 60) + (end_time[1] * 60 * 60) + (end_time[2] * 60) + end_time[3];
-
-    // Calculate the event duration in seconds
-    long long duration_seconds = end_seconds - begin_seconds;
-
-    // Calculate days, hours, minutes, and seconds
-    int days = duration_seconds / (24 * 60 * 60);
-    duration_seconds %= 24 * 60 * 60;
-    int hours = duration_seconds / (60 * 60);
-    duration_seconds %= 60 * 60;
-    int minutes = duration_seconds / 60;
-    int seconds = duration_seconds % 60;
-
-    // Output the result
-    printf("%d dia(s)\n%d hora(s)\n%d minuto(s)\n%d segundo(s)\n", days, hours, minutes, seconds);
-
+    int parsed_values1 = sscanf(init_time, "%d : %d : %d", &init_hours, &init_minutes, &init_seconds);
+    
+    //if (parsed_values1 == 3) {
+        printf("Hours: %d, Minutes: %d, Seconds: %d\n", init_hours, init_minutes, init_seconds);
+    //} else {
+   //     printf("Invalid time format.\n");
+   // }
+    
     return 0;
 }
